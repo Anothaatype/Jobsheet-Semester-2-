01cc2dc25919a4b248aba9db2b10e4d01881a81c
Baskoro Seno Aji <baskorosenoaji@gmail.com>
2024-06-04 00:11:38 +0700

    task 2

---
+++ week10/Passengers.java
@@ -0,0 +1,14 @@
+package week10;
+
+public class Passengers {
+        String name,cityOrigin,cityDestination;
+        int ticketAmount,price;
+    
+        public Passengers(String name, String cityOrigin, String cityDestination, int ticketAmount, int price) {
+            this.name = name;
+            this.cityOrigin = cityOrigin;
+            this.cityDestination = cityDestination;
+            this.ticketAmount = ticketAmount;
+            this.price = price;
+        }
+    }
