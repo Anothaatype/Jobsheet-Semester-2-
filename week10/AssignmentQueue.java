675e7cd7893fd25a9b96d53e01c46ab36df8ffe5
Baskoro Seno Aji <baskorosenoaji@gmail.com>
2024-06-04 09:43:21 +0700

    hh

---
+++ week10/AssignmentQueue.java
@@ -0,0 +1,15 @@
+package week10;
+
+public class AssignmentQueue {
+        String name,
+        nim;
+        int classNumber;
+        double gpa;
+    
+        public AssignmentQueue(String name, String nim, int classNumber, double gpa) {
+            this.name = name;
+            this.nim = nim;
+            this.classNumber = classNumber;
+            this.gpa = gpa;
+        }
+    }
